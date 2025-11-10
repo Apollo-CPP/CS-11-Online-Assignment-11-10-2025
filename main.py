c946e954762fd5a94da700add73df1d12bb395a8
Apollo-CPP <dylanzhangrblx2010@gmail.com>
2025-11-10 22:39:14 +0000

    Starting to learn Python :-D

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+while True:
+    Name = input("Hello! What is your name?: ")
 
+    if not Name or len(Name) == 0 or Name.isspace():
+        print("You inputted nothing or you put only put spaces >:(")
+        continue
+    else:
+        print(f"{Name} is cool!")
+        break
